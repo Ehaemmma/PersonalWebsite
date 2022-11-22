67e3caf11b80c0dc5f63146d92410785b6551a72
Ehaema <ehaema@umich.edu>
2022-11-21 22:39:28 -0500

    Setup 1.1

---
+++ text.js
@@ -0,0 +1,8 @@
+document.getElementById("intro").innerHTML =
+    "It is easy to describe <mark id='whoIam'>who I am</mark>, but hard on what I do. " + "<br />" +
+    "I study Electrical & Computer Engineering and Industrial Engineering," + "<br />" +
+    "and then make arts, direct plays, develop games. Sometimes I play with lights." + "<br />" +
+    "It is so fun to connect different knowledge of the time with stories." + "<br />" +
+    "So that is what I do: I create and discover complex systems, " + "<br />" +
+    "which I don't necessarily solve. I create lenses to look at them." + "<br />" +
+    "Always believe in the spark of interdisciplinary and teamwork." + "<br />";
